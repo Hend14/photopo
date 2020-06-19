b73308741e458548f51615824a63756c81855809
Hend14 <hendtaki1418@gmail.com>
2020-06-19 03:09:43 +0000

    fix config/app

--- config/app.php
+++ config/app.php
@@ -165,11 +165,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
-         * Package Service Providers...
-         */
-        kanazaca\CounterCache\CounterCacheServiceProvider::class,
-
-        /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
